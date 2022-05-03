f9ba74caf581e1e9634524fe227c274a5d3b51cb
Roberto Santos <rss11@dmz.org.br>
2022-05-02 22:42:55 -0300

    Criando o projeto, Definição do modelo, servico, utilidade e nuget lib NewtonsoftJson.

---
+++ Projeto/ConsoleApp/ConsoleApp/Servico/IElevadorServico.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Servico
+{
+    public interface IElevadorServico
+    {
+        List<int> andarMenosUtilizado();
+
+        List<char> elevadorMaisFrequentado();
+
+        List<char> periodoMaiorFluxoElevadorMaisFrequentado();
+
+        List<char> elevadorMenosFrequentado();
+
+        List<char> periodoMenorFluxoElevadorMenosFrequentado();
+
+        List<char> periodoMaiorUtilizacaoConjuntoElevadores();
+
+        float percentualDeUsoElevadorA();
+
+        float percentualDeUsoElevadorB();
+
+        float percentualDeUsoElevadorC();
+
+        float percentualDeUsoElevadorD();
+
+        float percentualDeUsoElevadorE();
+    }
+}
